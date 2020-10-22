2313c6dd2bf48fe16486821e7911284c647106ca
Muhammad Zahir Yusof <muhammad.zahir@inteksoft.com.my>
2020-10-22 12:54:25 +0800

    Update head.blade.php

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,6 +1,6 @@
-<link href="{{ asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
-<link href="{{ asset('assets/css/metismenu.min.css')}}" rel="stylesheet" type="text/css">
-<link href="{{ asset('assets/css/icons.css')}}" rel="stylesheet" type="text/css">
-<link href="{{ asset('assets/css/style.css')}}" rel="stylesheet" type="text/css">
+<link href="{{ URL::asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+<link href="{{ URL::asset('assets/css/metismenu.min.css')}}" rel="stylesheet" type="text/css">
+<link href="{{ URL::asset('assets/css/icons.css')}}" rel="stylesheet" type="text/css">
+<link href="{{ URL::asset('assets/css/style.css')}}" rel="stylesheet" type="text/css">
 
 @yield('css')
