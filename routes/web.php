f0d06facf60fb28afd6e2336cccfa73a1d2ec34a
Muhammad Zahir Yusof <muhammad.zahir@inteksoft.com.my>
2020-10-22 17:11:21 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::get('/', function () {
-        return view('auth.login');
+        return view('auth.register');
     });
 });
 
